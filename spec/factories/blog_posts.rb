f6e14157ee709cb4e99487e790f26287ebd92114
MohitBabbar1219 <mohit@mavenhive.in>
2019-06-27 13:30:34 +0530

    Add BlogPost Factory

--- spec/factories/blog_posts.rb
+++ spec/factories/blog_posts.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :blog_post do
     title { Faker::Lorem.word }
     body { Faker::Lorem.paragraph }
-    created_by {Faker::Number.number(10)}
+    author {Faker::Superhero.name}
   end
 end
