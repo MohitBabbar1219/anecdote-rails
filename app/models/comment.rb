2389bf5ea54a3aa018b65459bcdfa78f612eb2db
MohitBabbar1219 <mohit@mavenhive.in>
2019-06-27 15:08:52 +0530

    Add Comment model specifications

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :commentable, polymorphic: true
   has_many :comments, as: :commentable
+
+  validates_presence_of :commenter
+  validates_presence_of :content
+  validates_presence_of :commentable_id
+  validates_presence_of :commentable_type
 end
