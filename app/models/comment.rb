7c0cc74123754180745612faa277d0a5d452ba54
MohitBabbar1219 <mohit@mavenhive.in>
2019-06-28 14:39:25 +0530

    Add validation for presence of blog_post_id in columns table

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,6 +4,7 @@ class Comment < ApplicationRecord
 
   validates_presence_of :commenter
   validates_presence_of :content
+  validates_presence_of :blog_post_id
   validates_presence_of :commentable_id
   validates_presence_of :commentable_type
 end
