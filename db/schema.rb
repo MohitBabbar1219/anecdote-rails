7c66b23ee60db5b82cd09e060fa320c79fddf0d5
MohitBabbar1219 <mohit@mavenhive.in>
2019-06-28 14:38:44 +0530

    Add blog_post_id column to comments table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_27_075641) do
+ActiveRecord::Schema.define(version: 2019_06_27_121525) do
 
   create_table "blog_posts", force: :cascade do |t|
     t.string "title"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2019_06_27_075641) do
     t.string "commentable_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "blog_post_id"
   end
 
 end
